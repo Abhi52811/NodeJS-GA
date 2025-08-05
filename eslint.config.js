a4d9d13c266e2bb17322e8f1a8f7956bffe222f2
Abhi52811 <abhishekgupta52811@gmail.com>
2025-08-05 11:53:08 +0530

    added a 1st custom action

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,20 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
+const cleanGlobals = Object.fromEntries(
+  Object.entries(globals.browser).filter(
+    ([key]) => !key.includes("AudioWorkletGlobalScope")
+  )
+);
+
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: cleanGlobals,
+    },
+  },
   pluginReact.configs.flat.recommended,
 ]);
